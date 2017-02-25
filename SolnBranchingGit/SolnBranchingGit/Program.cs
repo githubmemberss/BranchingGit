ad19ebc65a330789c976a68977e9e2351cfb4524
Mangal Singh <mangalsinghgill21@gmail.com>
2017-02-25 23:36:37 +0530

    #mangal : FirstCommit

---
+++ SolnBranchingGit/SolnBranchingGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolnBranchingGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
